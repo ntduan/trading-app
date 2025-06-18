01869ccb0034cec23e60529a83ed04ba95f7691a
ntduan <nxiiun@gmail.com>
2025-06-18 13:34:33 +0800

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,79 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const [
+  typescriptEslintPlugin,
+  typescriptEslintParser,
+  prettierPlugin,
+  importPlugin,
+  reactPlugin,
+] = await Promise.all([
+  import('@typescript-eslint/eslint-plugin'),
+  import('@typescript-eslint/parser'),
+  import('eslint-plugin-prettier'),
+  import('eslint-plugin-import'),
+  import('eslint-plugin-react'),
+]);
+
+/** @type {import("eslint").Linter.Config[]} */
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  ...compat.extends(
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ),
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: typescriptEslintParser.default,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslintPlugin.default,
+      prettier: prettierPlugin.default,
+      import: importPlugin.default,
+      react: reactPlugin.default,
+    },
+    rules: {
+      'react/jsx-closing-bracket-location': ['error', 'line-aligned'],
+      'react/self-closing-comp': ['warn', { html: true, component: true }],
+      'prettier/prettier': [
+        'warn',
+        {
+          printWidth: 120,
+          endOfLine: 'lf',
+          singleQuote: true,
+          trailingComma: 'es5',
+        },
+      ],
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        {
+          prefer: 'type-imports',
+          fixStyle: 'inline-type-imports',
+        },
+      ],
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'import/order': [
+        'warn',
+        {
+          'newlines-between': 'always-and-inside-groups',
+          alphabetize: { order: 'asc' },
+        },
+      ],
+    },
+  },
+];
