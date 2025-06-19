c049510079f6c6e349096301846758b98688c228
ntduan <nxiiun@gmail.com>
2025-06-19 12:15:11 +0800

    feat: add theme switching functionality with next-themes and update styles

--- apps/trading-app/src/components/header/header.tsx
+++ apps/trading-app/src/components/header/header.tsx
@@ -1,19 +1,24 @@
 import Link from 'next/link';
 
+import { SwitchTheme } from './switch-theme';
+
 export const Header = () => {
   return (
-    <header className="bg-gray-800 text-white p-4">
-      <nav className="flex justify-between items-center">
-        <h1 className="text-xl font-bold">Trading App</h1>
-        <div className="flex space-x-4">
-          <Link href="/" className="hover:text-gray-300">
-            Home
-          </Link>
+    <header className="bg-card text-foreground px-6 py-5">
+      <div className="flex">
+        <h1 className="text-xl italic">Trading App</h1>
+        <nav className="flex space-x-4 text-xs items-center">
           <Link href="/trade" className="hover:text-gray-300">
             Trade
           </Link>
+          <Link href="/about" className="hover:text-gray-300">
+            About
+          </Link>
+        </nav>
+        <div className="flex items-center ml-auto">
+          <SwitchTheme />
         </div>
-      </nav>
+      </div>
     </header>
   );
 };
---
+++ apps/trading-app/src/components/header/switch-theme.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { Sun, Moon } from 'lucide-react';
+import { useTheme } from 'next-themes';
+
+import { useMounted } from '@/hooks/useIsMounted';
+import { cn } from '@/lib/utils';
+
+export const SwitchTheme = ({ className }: { className?: string }) => {
+  const { setTheme, theme } = useTheme();
+
+  const isMounted = useMounted();
+
+  if (!isMounted) return null;
+
+  return (
+    <div className={cn('cursor-pointer', className)} onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+      {theme === 'dark' ? <Sun /> : <Moon />}
+    </div>
+  );
+};
